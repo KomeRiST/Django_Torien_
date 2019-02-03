cae43df8fc3419618ff9cb6246351fa4458f34e8
KomeRiST <johndoe@example.comkomerist@bk.ru>
2019-02-03 22:04:22 +0500

    step1

--- DjangoTorien/DjangoTorien/settings.py
+++ DjangoTorien/DjangoTorien/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['app\media']))
+
